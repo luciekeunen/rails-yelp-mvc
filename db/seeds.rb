4f3223f2247266610074ca80711bfd6a073b5a89
Lucie Keunen <lucie@keunen.net>
2017-11-22 13:28:03 +0100

    Add a review for each restaurant in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
 require 'faker'
 
-["chinese", "italian", "japanese", "french", "belgian"]
-
 10.times do
-  Restaurant.create!(
+  restaurant = Restaurant.create!(
     name: Faker::Dessert.variety,
     address: Faker::Address.street_address,
     phone_number: Faker::PhoneNumber.phone_number,
     category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+  Review.create!(
+    content: Faker::HowIMetYourMother.quote,
+    rating: %w(0 1 2 3 4 5).sample,
+    restaurant_id: restaurant.id
   )
 end
